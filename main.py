5393e6eb0fbc5de46908b3761c9d73cac37d42a5
infrainfo-crypt <infrainfo60@gmail.com>
2026-09-24 22:05:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import requests
 
-url = "https://api.msrc.microsoft.com/cvrf/v3.0/Updates('2026-Sep')?api-version=2023-11-01"
+updates_url = "https://api.msrc.microsoft.com/cvrf/v3.0/Updates('2026-Sep')?api-version=2023-11-01"
 
 response = requests.get(
-    url,
+    updates_url,
     headers={"Accept": "application/json"},
     timeout=30
 )
 
 response.raise_for_status()
 
-data = response.json()
+updates = response.json()["value"]
 
 print("Microsoft Security Update Information")
 print("--------------------------------------")
-print(f"取得データの型: {type(data)}")
-print(f"取得データのキー: {list(data.keys())}")
 
-print("\n取得したデータ:")
-print(data)
+for update in updates:
+    print(f"ID: {update['ID']}")
+    print(f"Title: {update['DocumentTitle']}")
+    print(f"Initial Release: {update['InitialReleaseDate']}")
+    print(f"Current Release: {update['CurrentReleaseDate']}")
+    print(f"CVRF URL: {update['CvrfUrl']}")
+
+    cvrf_response = requests.get(
+        update["CvrfUrl"],
+        headers={"Accept": "application/json"},
+        timeout=30
+    )
+
+    cvrf_response.raise_for_status()
+
+    cvrf_data = cvrf_response.json()
+
+    print("\n取得した詳細データのキー:")
+    print(list(cvrf_data.keys()))
+
+    print("\n詳細データ:")
+    print(cvrf_data)
